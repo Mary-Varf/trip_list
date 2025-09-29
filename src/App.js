2a82d49bc416c41f6a29aed4d62a62305c88d75b
mary-varf <bma1989@yandex.ru>
2025-09-29 17:45:04 +0100

    add content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Pasport", quantity: 2, packed: false },
+  { id: 2, description: "dfg", quantity: 21, packed: true },
+  { id: 31, description: "sd", quantity: 21, packed: true },
+];
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <List />
+      <Stats />
     </div>
   );
-}
+};
+
+const Logo = () => {
+  return <h1>🌴Far Away💼</h1>;
+};
+const Form = () => {
+  const [input, setInput] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!input) return;
+
+    const newItem = {
+      description: input,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    setInput("");
+    setQuantity(1);
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do ou need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={input}
+        onChange={handleChange}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+const List = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+const handleDelete = () => {};
+const Item = ({ item }) => {
+  return (
+    <li style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={handleDelete}>&times;</button>
+    </li>
+  );
+};
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X(X %)</em>
+    </footer>
+  );
+};
 
 export default App;
